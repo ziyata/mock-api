ea1f1c45869113e4541a710cfadeea3d56b34d2d
aksa <akhsaniziyat@gmail.com>
2026-01-10 21:33:16 +0700

    fixing some issue

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
-    /**
-     * Disable CSRF for all requests (local mock only).
-     */
-    public function handle($request, Closure $next): Response
-    {
-        return $next($request);
-    }
+    protected $except = [
+        'api/*',
+        'api/create-gris',
+    ];
 }
